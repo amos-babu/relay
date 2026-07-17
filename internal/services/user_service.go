abacc26e41f261a31454f35a05ec3012beb41f84
amos-babu <amos.babu@yahoo.com>
2026-07-17 14:27:36 +0300

    user_service

--- internal/services/user_service.go
+++ internal/services/user_service.go
@@ -2,8 +2,12 @@ package services
 
 import (
 	"context"
+	"relay/internal/models"
 	"relay/internal/repositories"
 	"relay/internal/validation"
+	"strings"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 type UserService struct {
@@ -17,8 +21,28 @@ func NewUserService(repo repositories.UserRepository) *UserService {
 }
 
 func (s *UserService) Register(ctx context.Context, name string, email string, password string) error {
+	// validation
 	if err := validation.ValidateRegistraion(name, email, password); err != nil {
 		return err
 	}
-	return nil
+
+	// normalize email
+	email = strings.TrimSpace(strings.ToLower(email))
+	name = strings.TrimSpace(name)
+
+	// hash password
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+
+	// create user
+	user := &models.User{
+		Name:         name,
+		Email:        email,
+		PasswordHash: string(hash),
+	}
+
+	// save user
+	return s.repo.Create(ctx, user)
 }

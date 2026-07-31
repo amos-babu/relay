ae2d24860c7feb78de51fc81199128974ffc7247
amos-babu <amos.babu@yahoo.com>
2026-07-31 16:14:48 +0300

    "feat: Implimenting Ping pong wait"

--- internal/websocket/client.go
+++ internal/websocket/client.go
@@ -1,6 +1,8 @@
 package websocket
 
 import (
+	"time"
+
 	"github.com/gorilla/websocket"
 )
 
@@ -10,7 +12,22 @@ type Client struct {
 	Send   chan []byte
 }
 
+const (
+	writeWait  = 10 * time.Second
+	pongWait   = 60 * time.Second
+	pingPeriod = (pongWait * 9) / 10
+)
+
 func (c *Client) readPump() {
+	c.Conn.SetReadDeadline(
+		time.Now().Add(pongWait),
+	)
+	c.Conn.SetPongHandler(func(string) error {
+		return c.Conn.SetReadDeadline(
+			time.Now().Add(pongWait),
+		)
+	})
+	defer c.Conn.Close()
 	for {
 		_, _, err := c.Conn.ReadMessage()
 		if err != nil {
@@ -20,12 +37,26 @@ func (c *Client) readPump() {
 }
 
 func (c *Client) writePump() {
-	for message := range c.Send {
-		if err := c.Conn.WriteMessage(
-			websocket.TextMessage,
-			message,
-		); err != nil {
-			return
+	ticker := time.NewTicker(pingPeriod)
+	defer ticker.Stop()
+	for {
+		select {
+		case message, ok := <-c.Send:
+			c.Conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if !ok {
+				return
+			}
+			if err := c.Conn.WriteMessage(
+				websocket.TextMessage,
+				message,
+			); err != nil {
+				return
+			}
+		case <-ticker.C:
+			c.Conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if err := c.Conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+				return
+			}
 		}
 	}
 }
